8540da84274ee75f564d53762c65ddc67d67faa8
yanira <yanira.p@hotmail.com>
2021-07-04 19:15:55 +0200

    style: add some style GuestInfo & GuestForm

--- next/components/guestForm.js
+++ next/components/guestForm.js
@@ -1,53 +1,53 @@
-import { useState } from 'react';
-
 import styles from '../styles/GuestPage.module.css';
 
 //TODO: UPDATE CSS FILE
 export default function GuestForm({ guest, handleSubmit }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <h1>{guest.name}</h1>
-      </div>
-      <div>
-        <label>Are you attending the wedding? </label>
-        <select
-          name='rsvp'
-          defaultValue={guest.rsvp}
-          onChange={(e) => (guest.rsvp = e.target.value)}>
-          <option value='confirmed'>Yes</option>
-          <option value='unconfirmed'>No</option>
-        </select>
-      </div>
+    <div className={styles.formContainer}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h1>{guest.name}</h1>
+        </div>
+        <div className={styles.formLabelContainer}>
+          <label className={styles.formLabel}>Are you attending the wedding? </label>
+          <select
+            name='rsvp'
+            defaultValue={guest.rsvp}
+            onChange={(e) => (guest.rsvp = e.target.value)}>
+            <option value='confirmed'>Yes</option>
+            <option value='unconfirmed'>No</option>
+          </select>
+        </div>
 
-      <div>
-        <label>Do you come with a companion? </label>
-        <select
-          name='plusOne'
-          defaultValue={guest.plusOne}
-          onChange={(e) => (guest.plusOne = e.target.value)}>
-          <option value='true'>Yes</option>
-          <option value='false'>No</option>
-        </select>
-      </div>
-      <div>
-        <label>Please choose your menu: </label>
-        <br />
-        {guest.mealSelection.map((dish) => (
-          <div key={dish.dish}>
-            <input
-              type='checkbox'
-              name={dish.dish}
-              value={dish.selected}
-              onChange={() => (dish.selected = !dish.selected)}
-              checked={dish.selected}
-            />
-            <label htmlFor={dish.dish}>{dish.dish}</label>
-          </div>
-        ))}
-      </div>
+        <div className={styles.formLabelContainer}>
+          <label className={styles.formLabel}>Do you come with a companion? </label>
+          <select
+            name='plusOne'
+            defaultValue={guest.plusOne}
+            onChange={(e) => (guest.plusOne = e.target.value)}>
+            <option value='true'>Yes</option>
+            <option value='false'>No</option>
+          </select>
+        </div>
+        <div className={styles.formLabelContainer}>
+          <label className={styles.formLabel}>Please choose your menu: </label>
+          <br />
+          {guest.mealSelection.map((dish) => (
+            <div key={dish.dish}>
+              <input
+                type='checkbox'
+                name={dish.dish}
+                value={dish.selected}
+                onChange={() => (dish.selected = !dish.selected)}
+                checked={dish.selected}
+              />
+              <label htmlFor={dish.dish} className={styles.formLabel}>{dish.dish}</label>
+            </div>
+          ))}
+        </div>
 
-      <button type='submit'>Send</button>
-    </form>
+        <button className={styles.btn} type='submit'>Send</button>
+      </form>
+    </div>
   );
 }
--- next/components/guestInfo.js
+++ next/components/guestInfo.js
@@ -1,42 +1,44 @@
 import styles from '../styles/GuestPage.module.css';
 
-
 //TODO: UPDATE CSS FILE
-export default function GuestInfo({guest, handelEdit}) {
-  
+export default function GuestInfo({ guest, handelEdit }) {
   return (
-    <div>
-    <h2 className={styles.title}>
-      Hi {guest.name.split(' ').slice(0, 1)}!
-    </h2>
-      <p className={styles.question}>Would you like to edit your information?</p>
-    <> <button className={styles.btn} onClick={handelEdit}>
-     
+    <div className={styles.infoContainer}>
+      <h2 className={styles.title}>Hi {guest.name.split(' ').slice(0, 1)}!</h2>
+      <p className={styles.question}>
+        Would you like to edit your information?
+      </p>
+
+      <div className={styles.guestInfo}>
+        <div className={styles.guestInfoItem}> 
+        <p>Name: {guest.name}</p></div>
+        <div className={styles.guestInfoItem}>
+        <p> Plus one: {guest.plusOne ? 'Yes' : 'No'}</p>
+        </div>
+        <div className={styles.guestInfoItem}>
+        <p> RSVP:{' '}
+          {guest.rsvp === 'confirmed' ? guest.rsvp : 'pending confirmation'}</p>
+        </div>
+        <div className={styles.guestInfoItem}>
+         <p>Menu selection:{' '}
+          {!guest.mealSelection[0].selected &&
+          !guest.mealSelection[1].selected &&
+          !guest.mealSelection[2].selected
+            ? 'No menu'
+            : guest.mealSelection.map(
+                (dish, i) =>
+                  dish.selected && (
+                    <li key={i} className={styles.dish}>
+                      {'' + dish.dish + ' '}
+                    </li>
+                  )
+              )}</p> 
+        </div>
+      </div>
+
+      <button className={styles.btn} onClick={handelEdit}>
         Edit
       </button>
-    </>
-    <div className={styles.guestInfo}>
-      <div className={styles.guestInfoItem}>Name: {guest.name}</div>
-      <div className={styles.guestInfoItem}>
-        Plus one: {guest.plusOne ? 'Yes' : 'No'}
-      </div>
-      <div className={styles.guestInfoItem}>
-        RSVP:{' '}
-        {guest.rsvp === 'confirmed' ? guest.rsvp : 'pending confirmation'}
-      </div>
-      <div className={styles.guestInfoItem}>
-        Menu selection:{' '}
-        {guest.mealSelection.map(
-          (dish, i) =>
-            dish.selected && (
-              <span key={i} className={styles.dish}>
-                {'·' + dish.dish + ' '}
-              </span>
-            )
-        )}
-      </div>
     </div>
-  </div>
-
   );
 }
