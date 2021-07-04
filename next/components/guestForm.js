b03ef960163c33fd5ab6114abcae4b0a70df46a3
yanira <yanira.p@hotmail.com>
2021-07-04 18:02:09 +0200

    feat: add GuesForm funcionality

--- next/components/guestForm.js
+++ next/components/guestForm.js
@@ -1,8 +1,9 @@
+import { useState } from 'react';
+
 import styles from '../styles/GuestPage.module.css';
 
 //TODO: UPDATE CSS FILE
 export default function GuestForm({ guest, handleSubmit }) {
-
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -12,15 +13,19 @@ export default function GuestForm({ guest, handleSubmit }) {
         <label>Are you attending the wedding? </label>
         <select
           name='rsvp'
-          defaultValue={guest.rsvp === 'confirmed' ? true : false}>
-          <option value='true'>Yes</option>
-          <option value='false'>No</option>
+          defaultValue={guest.rsvp}
+          onChange={(e) => (guest.rsvp = e.target.value)}>
+          <option value='confirmed'>Yes</option>
+          <option value='unconfirmed'>No</option>
         </select>
       </div>
 
       <div>
         <label>Do you come with a companion? </label>
-        <select name='plusOne' defaultValue={guest.plusOne}>
+        <select
+          name='plusOne'
+          defaultValue={guest.plusOne}
+          onChange={(e) => (guest.plusOne = e.target.value)}>
           <option value='true'>Yes</option>
           <option value='false'>No</option>
         </select>
@@ -29,10 +34,16 @@ export default function GuestForm({ guest, handleSubmit }) {
         <label>Please choose your menu: </label>
         <br />
         {guest.mealSelection.map((dish) => (
-          <>
-            <input type='checkbox' name={dish.dish} value={dish.dish} checked={dish.selected}/>
+          <div key={dish.dish}>
+            <input
+              type='checkbox'
+              name={dish.dish}
+              value={dish.selected}
+              onChange={() => (dish.selected = !dish.selected)}
+              checked={dish.selected}
+            />
             <label htmlFor={dish.dish}>{dish.dish}</label>
-          </>
+          </div>
         ))}
       </div>
 
--- next/pages/guests/[id].js
+++ next/pages/guests/[id].js
@@ -1,12 +1,10 @@
 import { useRouter } from 'next/router';
-import useSWR from 'swr';
-import styles from '../../styles/GuestPage.module.css';
-import editIcon from '../../public/edit.svg';
-
-import { fetcher } from '../index';
 import { useState } from 'react';
+import useSWR from 'swr';
+import { fetcher } from '../index';
 import GuestInfo from '../../components/guestInfo';
 import GuestForm from '../../components/guestForm';
+import styles from '../../styles/GuestPage.module.css';
 
 //TODO-> MENU SELETION: MESSAGE NO MENU SELECTED
 export default function GuestPage() {
@@ -17,45 +15,23 @@ export default function GuestPage() {
 
   const [isEditing, setEditing] = useState(false);
 
-  const [query, setQuery] = useState({
-    name: "",
-  });
-
   if (error) return 'An error has occurred: ' + error;
   if (!data) return 'Loading...';
 
-  // const handleParam = () => (e) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   setQuery((prevState) => ({
-  //     ...prevState,
-  //     [name]: value,
-  //   }));
-  // };
-
-  // Form Submit function
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    Object.entries(query).forEach(([key, value]) => {
-      formData.append(key, value);
-    });
-    fetch(`/api/guests/${id}`, {
-      method: 'POST',
-      body: formData,
-    }).then(() => setQuery({ name: '' }));
-    router.push('/')
+    router.push('/');
   };
 
-const handelEdit = () => setEditing(true);
+  const handelEdit = () => setEditing(true);
 
   return (
     <div className={styles.GuestPageCointaner}>
       {!isEditing ? (
         <GuestInfo guest={data} handelEdit={handelEdit} />
       ) : (
-             <GuestForm guest={data} handleSubmit={handleSubmit} />
-       )}
+        <GuestForm guest={data} handleSubmit={handleSubmit} />
+      )}
     </div>
   );
 }
